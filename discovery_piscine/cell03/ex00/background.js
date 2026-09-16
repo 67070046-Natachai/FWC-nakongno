b170caf74e2e1ddfe7154a3bdafa29072c46c883
67070046-Natachai <176611935+67070046-Natachai@users.noreply.github.com>
2026-09-16 16:57:15 +0700

    Create background.js

---
+++ discovery_piscine/cell03/ex00/background.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("changeColor");
+
+button.onclick = function() {
+    let color = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + color;
+};
